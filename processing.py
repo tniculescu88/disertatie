b9d9310c7622615eb1aff5e82219211a70331e9d
eca399 <ecaterinaparlog@gmail.com>
2018-08-28 12:00:38 +0300

    user_loc & use-loc-clustered csv in processing

--- processing.py
+++ processing.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import json
+
 
 with open('transition_mat.json', 'r') as f:
     transition_mat = json.load(f)
@@ -8,6 +10,10 @@ with open('transition_list.json', 'r') as f:
    
 with open('sp_trans_list.json', 'r') as f:
     sp_trans_list = json.load(f)
+    
+df_gps = pd.read_csv('user_location.csv')  
+
+df_clustered = pd.read_csv('user-location-clustered.csv')  
     
 
 print('loaded transition matrix is {}'.format(transition_mat))
